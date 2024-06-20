e0daf24f12226842b8828ccb8a4c5f6d6dc45870
BetazoDev <75762984+BetazoDev@users.noreply.github.com>
2024-06-20 09:53:13 -0600

    Create BetazoDev.js
    JavaScript 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/BetazoDev.js
@@ -0,0 +1,25 @@
+//Sitio web oficial del lenguaje: https://www.javascript.com/
+
+//Comentarios de una sola línea
+
+/* 
+    comentarios en 
+    varias líneas
+*/
+
+var x = 1;
+const pi = 3.1416;
+
+var aString = "JavaScript";
+var aNumber = 4;
+var aBooleanTrue = true;
+var aBooleanFalse= false;   
+var aNull = null;
+var anUndefined = undefined;
+const anObject = {
+    name: "Juan",
+    age: 23,
+    male: true
+}
+console.log("¡Hola JavaScript!");
+
